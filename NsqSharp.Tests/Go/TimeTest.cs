818855d4587d7b9389a02dd712a8cf4e8fcf0404
Jud White <github@judsonwhite.com>
2015-01-28 12:52:41 -0600

    indentation

--- NsqSharp.Tests/Go/TimeTest.cs
+++ NsqSharp.Tests/Go/TimeTest.cs
@@ -10,57 +10,57 @@ namespace NsqSharp.Tests.Go
     {
         private static readonly TestData<string, long> _parseDurationTests = new TestData<string, long>
         {
-	        // simple
-	        {"0", new Result<long>(0)},
-	        {"5s", new Result<long>(5 * Time.Second)},
-	        {"30s", new Result<long>(30 * Time.Second)},
-	        {"1478s", new Result<long>(1478 * Time.Second)},
-	        // sign
-	        {"-5s", new Result<long>(-5 * Time.Second)},
-	        {"+5s", new Result<long>(5 * Time.Second)},
-	        {"-0", new Result<long>(0)},
-	        {"+0", new Result<long>(0)},
-	        // decimal
-	        {"5.0s", new Result<long>(5 * Time.Second)},
-	        {"5.6s", new Result<long>(5 * Time.Second + 600 * Time.Millisecond)},
-	        {"5.s", new Result<long>(5 * Time.Second)},
-	        {".5s", new Result<long>(500 * Time.Millisecond)},
-	        {"1.0s", new Result<long>(1 * Time.Second)},
-	        {"1.00s", new Result<long>(1 * Time.Second)},
-	        {"1.004s", new Result<long>(1 * Time.Second + 4 * Time.Millisecond)},
-	        {"1.0040s", new Result<long>(1 * Time.Second + 4 * Time.Millisecond)},
-	        {"100.00100s", new Result<long>(100 * Time.Second + 1 * Time.Millisecond)},
-	        // different units
-	        {"10ns", new Result<long>(10 * Time.Nanosecond)},
-	        {"11us", new Result<long>(11 * Time.Microsecond)},
-	        {"12µs", new Result<long>(12 * Time.Microsecond)}, // U+00B5
-	        {"12μs", new Result<long>(12 * Time.Microsecond)}, // U+03BC
-	        {"13ms", new Result<long>(13 * Time.Millisecond)},
-	        {"14s", new Result<long>(14 * Time.Second)},
-	        {"15m", new Result<long>(15 * Time.Minute)},
-	        {"16h", new Result<long>(16 * Time.Hour)},
-	        // composite durations
-	        {"3h30m", new Result<long>(3 * Time.Hour + 30 * Time.Minute)},
-	        {"10.5s4m", new Result<long>(4 * Time.Minute + 10 * Time.Second + 500 * Time.Millisecond)},
-	        {"-2m3.4s", new Result<long>(-(2 * Time.Minute + 3 * Time.Second + 400 * Time.Millisecond))},
-	        {"1h2m3s4ms5us6ns", new Result<long>(1 * Time.Hour + 2 * Time.Minute + 3 * Time.Second + 4 * Time.Millisecond + 
+            // simple
+            {"0", new Result<long>(0)},
+            {"5s", new Result<long>(5 * Time.Second)},
+            {"30s", new Result<long>(30 * Time.Second)},
+            {"1478s", new Result<long>(1478 * Time.Second)},
+            // sign
+            {"-5s", new Result<long>(-5 * Time.Second)},
+            {"+5s", new Result<long>(5 * Time.Second)},
+            {"-0", new Result<long>(0)},
+            {"+0", new Result<long>(0)},
+            // decimal
+            {"5.0s", new Result<long>(5 * Time.Second)},
+            {"5.6s", new Result<long>(5 * Time.Second + 600 * Time.Millisecond)},
+            {"5.s", new Result<long>(5 * Time.Second)},
+            {".5s", new Result<long>(500 * Time.Millisecond)},
+            {"1.0s", new Result<long>(1 * Time.Second)},
+            {"1.00s", new Result<long>(1 * Time.Second)},
+            {"1.004s", new Result<long>(1 * Time.Second + 4 * Time.Millisecond)},
+            {"1.0040s", new Result<long>(1 * Time.Second + 4 * Time.Millisecond)},
+            {"100.00100s", new Result<long>(100 * Time.Second + 1 * Time.Millisecond)},
+            // different units
+            {"10ns", new Result<long>(10 * Time.Nanosecond)},
+            {"11us", new Result<long>(11 * Time.Microsecond)},
+            {"12µs", new Result<long>(12 * Time.Microsecond)}, // U+00B5
+            {"12μs", new Result<long>(12 * Time.Microsecond)}, // U+03BC
+            {"13ms", new Result<long>(13 * Time.Millisecond)},
+            {"14s", new Result<long>(14 * Time.Second)},
+            {"15m", new Result<long>(15 * Time.Minute)},
+            {"16h", new Result<long>(16 * Time.Hour)},
+            // composite durations
+            {"3h30m", new Result<long>(3 * Time.Hour + 30 * Time.Minute)},
+            {"10.5s4m", new Result<long>(4 * Time.Minute + 10 * Time.Second + 500 * Time.Millisecond)},
+            {"-2m3.4s", new Result<long>(-(2 * Time.Minute + 3 * Time.Second + 400 * Time.Millisecond))},
+            {"1h2m3s4ms5us6ns", new Result<long>(1 * Time.Hour + 2 * Time.Minute + 3 * Time.Second + 4 * Time.Millisecond + 
                                                  5 * Time.Microsecond + 6 * Time.Nanosecond)},
-	        {"39h9m14.425s", new Result<long>(39 * Time.Hour + 9 * Time.Minute + 14 * Time.Second + 425 * Time.Millisecond)},
-	        // large value
-	        {"52763797000ns", new Result<long>(52763797000 * Time.Nanosecond)},
-	        // more than 9 digits after decimal point, see http://golang.org/issue/6617
-	        {"0.3333333333333333333h", new Result<long>(20 * Time.Minute)},
+            {"39h9m14.425s", new Result<long>(39 * Time.Hour + 9 * Time.Minute + 14 * Time.Second + 425 * Time.Millisecond)},
+            // large value
+            {"52763797000ns", new Result<long>(52763797000 * Time.Nanosecond)},
+            // more than 9 digits after decimal point, see http://golang.org/issue/6617
+            {"0.3333333333333333333h", new Result<long>(20 * Time.Minute)},
 
-	        // errors
-	        {"", new Result<long, InvalidDataException>()},
-	        {"3", new Result<long, InvalidDataException>()},
-	        {"-", new Result<long, InvalidDataException>()},
-	        {"s", new Result<long, InvalidDataException>()},
-	        {".", new Result<long, InvalidDataException>()},
-	        {"-.", new Result<long, InvalidDataException>()},
-	        {".s", new Result<long, InvalidDataException>()},
-	        {"+.s", new Result<long, InvalidDataException>()},
-	        {"3000000h", new Result<long, OverflowException>()}, // overflow
+            // errors
+            {"", new Result<long, InvalidDataException>()},
+            {"3", new Result<long, InvalidDataException>()},
+            {"-", new Result<long, InvalidDataException>()},
+            {"s", new Result<long, InvalidDataException>()},
+            {".", new Result<long, InvalidDataException>()},
+            {"-.", new Result<long, InvalidDataException>()},
+            {".s", new Result<long, InvalidDataException>()},
+            {"+.s", new Result<long, InvalidDataException>()},
+            {"3000000h", new Result<long, OverflowException>()}, // overflow
             {"5.6x", new Result<long, InvalidDataException>()}, // bad unit
             {"9223372036854775806ns", new Result<long, OverflowException>()}, // overflow in leadingInt
         };
