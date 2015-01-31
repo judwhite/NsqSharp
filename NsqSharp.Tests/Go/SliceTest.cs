dab5730f64228331a726d1082874153a63e1f01e
Jud White <github@judsonwhite.com>
2015-01-31 03:20:58 -0600

    slice fix, slice unit tests

---
+++ NsqSharp.Tests/Go/SliceTest.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using NsqSharp.Go;
+using NUnit.Framework;
+
+namespace NsqSharp.Tests.Go
+{
+    [TestFixture]
+    public class SliceTest
+    {
+        [Test]
+        public void TestStringEquality()
+        {
+            Slice<char> x = new Slice<char>("abcdef");
+            string y = "abcdef";
+
+            Assert.IsTrue(x == y);
+        }
+
+        [Test]
+        public void TestStringEqualityAfterSlice()
+        {
+            Slice<char> x = new Slice<char>("abcdef");
+            x = x.Slc(0, 3);
+            string y = "abc";
+
+            Assert.IsTrue(x == y);
+        }
+
+        [Test]
+        public void TestStringEqualityAfterMidSlice()
+        {
+            Slice<char> x = new Slice<char>("abcdef");
+            x = x.Slc(1, 4);
+            string y = "bcd";
+
+            Assert.IsTrue(x == y);
+        }
+
+        [Test]
+        public void TestStringTypeEqualityMismatch()
+        {
+            Slice<int> x = new Slice<int>(new[] { 1, 2, 3 });
+            string y = "abc";
+
+            Assert.IsFalse(x == y);
+        }
+
+        [Test]
+        public void TestNullStringEquality()
+        {
+            Slice<char> x = null;
+            string y = null;
+
+            Assert.IsTrue(x == y);
+        }
+
+        [Test]
+        public void TestRightNullStringInequality()
+        {
+            Slice<char> x = new Slice<char>("abc");
+            string y = null;
+
+            Assert.IsTrue(x != y);
+        }
+
+        [Test]
+        public void TestLeftNullSliceInequality()
+        {
+            Slice<char> x = null;
+            string y = "abc";
+
+            Assert.IsTrue(x != y);
+        }
+
+        [Test]
+        public void TestToStringOnSliceChar()
+        {
+            Slice<char> x = new Slice<char>("hello world");
+            var actual = x.Slc(2, 8).ToString();
+
+            Assert.AreEqual("llo wo", actual);
+        }
+
+        [Test]
+        public void TestToStringOnSliceInt()
+        {
+            Slice<int> x = new Slice<int>(new[] { 1, 2, 3, 4 });
+            string actual = x.ToString();
+
+            Assert.AreEqual("[ 1 2 3 4 ]", actual);
+        }
+
+        [Test]
+        public void TestSlcHasNoSideEffects()
+        {
+            Slice<char> x = new Slice<char>("hello world");
+            x.Slc(2, 8);
+
+            Assert.AreEqual("hello world".Length, x.Len());
+            Assert.AreEqual("hello world", x.ToString());
+        }
+
+        [Test]
+        public void TestSliceStartLessThan0()
+        {
+            Slice<char> x = new Slice<char>("test");
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => x.Slc(-1));
+        }
+
+        [Test]
+        public void TestSliceStartGreaterThanLen()
+        {
+            Slice<char> x = new Slice<char>("test");
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => x.Slc(5));
+        }
+
+        [Test]
+        public void TestSliceStartGreaterThanEnd()
+        {
+            Slice<char> x = new Slice<char>("test");
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => x.Slc(2, 1));
+        }
+
+        [Test]
+        public void TestSliceEndGreaterThanLen()
+        {
+            Slice<char> x = new Slice<char>("test");
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => x.Slc(2, 5));
+        }
+
+        [Test]
+        public void TestSliceEndGreaterThanLenAfterReslice()
+        {
+            Slice<char> x = new Slice<char>("test");
+
+            x = x.Slc(0, 2);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => x.Slc(0, 3));
+        }
+
+        [Test]
+        public void TestSliceAtZero()
+        {
+            Slice<char> x = new Slice<char>("test!");
+            x = x.Slc(0, 1);
+
+            Assert.AreEqual("t", x.ToString());
+        }
+
+        [Test]
+        public void TestSliceAtEnd()
+        {
+            Slice<char> x = new Slice<char>("test!");
+            x = x.Slc(x.Len() - 1);
+
+            Assert.AreEqual("!", x.ToString());
+        }
+
+        [Test]
+        public void TestEmptySliceAtZero()
+        {
+            Slice<char> x = new Slice<char>("test!");
+            x = x.Slc(0, 0);
+
+            Assert.AreEqual("", x.ToString());
+        }
+
+        [Test]
+        public void TestEmptySliceAtEnd()
+        {
+            Slice<char> x = new Slice<char>("test!");
+            x = x.Slc(x.Len());
+
+            Assert.AreEqual("", x.ToString());
+        }
+
+        [Test]
+        public void TestEmptySliceAtMid()
+        {
+            Slice<int> x = new Slice<int>(new[] { 1, 2, 3, 4 });
+            x = x.Slc(1, 1);
+
+            Assert.IsEmpty(x.ToArray());
+        }
+
+        [Test]
+        public void TestStringConstructorThrowsOnNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Slice<char>((string)null));
+        }
+
+        [Test]
+        public void TestStringConstructorThrowsOnTypeMistmatch()
+        {
+            Assert.Throws<Exception>(() => new Slice<int>("nope"));
+        }
+
+        [Test]
+        public void TestArrayConstructorThrowsOnNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Slice<int>((int[])null));
+        }
+    }
+}
--- NsqSharp/Go/Slice.cs
+++ NsqSharp/Go/Slice.cs
@@ -2,12 +2,20 @@
 
 namespace NsqSharp.Go
 {
-    internal class Slice<T>
+    /// <summary>
+    /// Slice
+    /// </summary>
+    /// <typeparam name="T">The type of data stored in the slice</typeparam>
+    public class Slice<T>
     {
         private readonly T[] _array;
         private int _offset;
         private int _maxIndex;
 
+        /// <summary>
+        /// Initializes a new Slice from a string
+        /// </summary>
+        /// <param name="value">The string</param>
         public Slice(string value)
         {
             if (value == null)
@@ -20,6 +28,10 @@ namespace NsqSharp.Go
             _maxIndex = _array.Length;
         }
 
+        /// <summary>
+        /// Initializes a new Slice from an array.
+        /// </summary>
+        /// <param name="array"></param>
         public Slice(T[] array)
         {
             if (array == null)
@@ -30,20 +42,22 @@ namespace NsqSharp.Go
             _maxIndex = array.Length;
         }
 
+        /// <summary>
+        /// Creates a new slice starting at <paramref name="start"/> and going to the end.
+        /// </summary>
+        /// <param name="start">The start index.</param>
+        /// <returns>A new slice.</returns>
         public Slice<T> Slc(int start)
         {
-            if (start < 0)
-                throw new ArgumentOutOfRangeException("start", start, "start must be >= 0");
-            if (start > Len())
-                throw new ArgumentOutOfRangeException("start", start, string.Format("start must be <= {0}", Len()));
-
-            var slice = new Slice<T>(_array);
-            slice._offset = _offset + start;
-            slice._maxIndex = _maxIndex;
-
-            return slice;
+            return Slc(start, Len());
         }
 
+        /// <summary>
+        /// Creates a new slice starting at <paramref name="start"/> and going to <paramref name="end"/>, exclusive.
+        /// </summary>
+        /// <param name="start">The start index.</param>
+        /// <param name="end">The end index + 1. (ex: Slc(2, 5) returns a new slice of length 3 going from index 2 to 4)</param>
+        /// <returns>A new slice.</returns>
         public Slice<T> Slc(int start, int end)
         {
             if (start < 0)
@@ -57,11 +71,14 @@ namespace NsqSharp.Go
 
             var slice = new Slice<T>(_array);
             slice._offset = _offset + start;
-            slice._maxIndex = slice._offset + end;
+            slice._maxIndex = _offset + end;
 
             return slice;
         }
 
+        /// <summary>
+        /// Returns the string if the slice is of type Slice&lt;char&gt; otherwise, returns a string representing the array.
+        /// </summary>
         public override string ToString()
         {
             if (typeof(T) == typeof(char))
@@ -69,9 +86,12 @@ namespace NsqSharp.Go
                 return new string((char[])(object)ToArray());
             }
 
-            return base.ToString();
+            return string.Format("[ {0} ]", string.Join(" ", ToArray()));
         }
 
+        /// <summary>
+        /// Returns the slice as an array.
+        /// </summary>
         public T[] ToArray()
         {
             T[] array = new T[Len()];
@@ -84,11 +104,18 @@ namespace NsqSharp.Go
             return array;
         }
 
+        /// <summary>
+        /// Returns the length of the slice.
+        /// </summary>
         public int Len()
         {
             return _maxIndex - _offset;
         }
 
+        /// <summary>
+        /// Gets the data at the specified index.
+        /// </summary>
+        /// <param name="index">The index of the data to return.</param>
         public T this[int index]
         {
             get
@@ -99,6 +126,12 @@ namespace NsqSharp.Go
             }
         }
 
+        /// <summary>
+        /// Checks if a slice and string are equal.
+        /// </summary>
+        /// <param name="s1">The first slice.</param>
+        /// <param name="s2">The second slice.</param>
+        /// <returns><c>true</c> if the slice and string are equal; otherwise, <c>false</c>.</returns>
         public static bool operator ==(Slice<T> s1, string s2)
         {
             if (ReferenceEquals(s1, null) && s2 == null)
@@ -112,21 +145,38 @@ namespace NsqSharp.Go
             return str1 == s2;
         }
 
+        /// <summary>
+        /// Checks if a slice and string are not equal.
+        /// </summary>
+        /// <param name="s1">The first slice.</param>
+        /// <param name="s2">The second slice.</param>
+        /// <returns><c>true</c> if the slice and string are not equal; otherwise, <c>false</c>.</returns>
         public static bool operator !=(Slice<T> s1, string s2)
         {
             return !(s1 == s2);
         }
 
+        /// <summary>
+        /// Compares strings and references, otherwise always returns <c>false</c>. This implementation may change.
+        /// </summary>
+        /// <param name="obj">The object to check equality with.</param>
+        /// <returns><c>true</c> if the strings or references are equal; otherwise, <c>false</c>.</returns>
         public override bool Equals(object obj)
         {
             string str = obj as string;
             if (this == str)
                 return true;
 
+            if (ReferenceEquals(this, obj))
+                return true;
+
             return false;
             // TODO: check other slices, arrays.. maybe
         }
 
+        /// <summary>
+        /// Default GetHashCode implementation.
+        /// </summary>
         public override int GetHashCode()
         {
             return base.GetHashCode();
