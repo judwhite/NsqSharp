1f8fe144b2984ea82577c24320f27b890aee80a8
Jud White <github@judsonwhite.com>
2015-02-02 23:03:58 -0600

    OptAttributeExtensions tests

---
+++ NsqSharp.Tests/Extensions/OptAttributeExtensionsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NsqSharp.Attributes;
+using NsqSharp.Extensions;
+using NUnit.Framework;
+
+namespace NsqSharp.Tests.Extensions
+{
+    [TestFixture]
+    public class OptAttributeExtensionsTest
+    {
+        [Test]
+        public void Coerce()
+        {
+            var opt = new OptAttribute("testName");
+            var coercedVal = opt.Coerce("12ms", typeof(TimeSpan));
+            Assert.AreEqual(TimeSpan.FromMilliseconds(12), coercedVal);
+        }
+
+        [Test]
+        public void InvalidCoerceThrows()
+        {
+            var opt = new OptAttribute("testName");
+
+            var ex = Assert.Throws<Exception>(() => opt.Coerce("12xs", typeof (TimeSpan)));
+            Assert.IsNotNull(ex);
+            Assert.IsNotNull(ex.Message);
+            Assert.IsTrue(ex.Message.Contains(opt.Name));
+        }
+    }
+}
--- NsqSharp/Extensions/OptAttributeExtensions.cs
+++ NsqSharp/Extensions/OptAttributeExtensions.cs
@@ -3,8 +3,18 @@ using NsqSharp.Attributes;
 
 namespace NsqSharp.Extensions
 {
-    internal static class OptAttributeExtensions
+    /// <summary>
+    /// <see cref="OptAttribute"/> Extensions.
+    /// </summary>
+    public static class OptAttributeExtensions
     {
+        /// <summary>
+        /// Coerce a value to the specified <paramref name="targetType"/>. Supports Duration and Bool string/int formats.
+        /// </summary>
+        /// <param name="opt">The <see cref="OptAttribute.Name"/> to throw in the exception if coerce fails.</param>
+        /// <param name="value">The value to coerce.</param>
+        /// <param name="targetType">The target type.</param>
+        /// <returns>The value as the <paramref name="targetType"/>.</returns>
         public static object Coerce(this OptAttribute opt, object value, Type targetType)
         {
             try
