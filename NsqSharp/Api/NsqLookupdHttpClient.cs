f7b91ea9576e5d202b554e64dcc1817d13e06e8d
Jud White <github@judsonwhite.com>
2015-08-17 05:07:07 -0500

    NsqLookupdClient: add method stubs for nsqlookupd api

--- NsqSharp/Api/NsqLookupdHttpClient.cs
+++ NsqSharp/Api/NsqLookupdHttpClient.cs
@@ -12,5 +12,57 @@ namespace NsqSharp.Api
             : base(nsqlookupdHttpAddress, httpRequestTimeout)
         {
         }
+
+        // TODO: Finish implementations
+
+        /// <summary>Returns a list of nsqd producers for a topic.</summary>
+        /// <param name="topic">The topic to list producers for.</param>
+        private void GetLookup(string topic)
+        {
+            ValidateTopic(topic);
+
+            var json = Get(string.Format("/lookup?topic={0}", topic));
+        }
+
+        /// <summary>Returns a list of all known topics.</summary>
+        private string[] GetTopics()
+        {
+            var json = Get(string.Format("/topics"));
+            return new[] { "TODO" };
+        }
+
+        /// <summary>Returns a list of all known channels of a <paramref name="topic"/>.</summary>
+        /// <param name="topic">The topic to list channels for.</param>
+        private void GetChannels(string topic)
+        {
+            ValidateTopic(topic);
+
+            var json = Get(string.Format("/channels?topic={0}", topic));
+        }
+
+        /// <summary>Returns a list of all known nsqd nodes.</summary>
+        private void GetNodes()
+        {
+            var json = Get(string.Format("/nodes"));
+        }
+
+        /// <summary>
+        ///     Tombstones a specific <paramref name="nsqdNode"/> producer of an existing <paramref name="topic"/>.
+        ///     Tombstoning a <paramref name="topic"/>
+        ///     prevents clients from discovering the <paramref name="nsqdNode"/> through nsqlookupd for a configurable --
+        ///     tombstone-liftime, allowing the <paramref name="nsqdNode"/>
+        ///     to delete the topic without it being recreated by connecting clients.
+        /// </summary>
+        /// <param name="topic">The topic to list producers for.</param>
+        /// <param name="nsqdNode">The nsqd node.</param>
+        private void TombstoneTopicProducer(string topic, string nsqdNode)
+        {
+            if (string.IsNullOrEmpty(nsqdNode))
+                throw new ArgumentNullException("nsqdNode");
+
+            ValidateTopic(topic);
+
+            var json = Get(string.Format("/tombstone_topic_producer?topic={0}&node={1}", topic, nsqdNode));
+        }
     }
 }
