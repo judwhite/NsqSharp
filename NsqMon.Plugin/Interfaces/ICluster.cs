c035f5e74a2e0563b9d9d78660e0fc6c6d01586c
Jud White <github@judsonwhite.com>
2015-08-19 03:52:33 -0500

    add nsqmon plugin interfaces

---
+++ NsqMon.Plugin/Interfaces/ICluster.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace NsqMon.Plugin.Interfaces
+{
+    /// <summary>Specifies an NSQ Cluster, a group of nsqd and nsqlookupd instances.</summary>
+    public interface ICluster
+    {
+        /// <summary>Gets the cluster's user-specified unique identifier.</summary>
+        /// <value>The cluster's user-specified unique identifier.</value>
+        Guid Id { get; }
+
+        /// <summary>Gets the cluster's display name.</summary>
+        /// <value>The cluster's display name.</value>
+        string DisplayName { get; }
+
+        /// <summary>Gets the environments associated with this cluster.</summary>
+        /// <returns>The environments associated with this cluster.</returns>
+        Collection<IEnvironment> GetEnvironments();
+    }
+}
---
+++ NsqMon.Plugin/Interfaces/IEnvironment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace NsqMon.Plugin.Interfaces
+{
+    /// <summary>
+    ///     Specifies an environment, such as localhost, staging, or production. Each environment uniquely belongs to a
+    ///     cluster.
+    /// </summary>
+    public interface IEnvironment
+    {
+        /// <summary>Gets the environment's display user-specified unique identifier.</summary>
+        /// <value>The environment's display user-specified unique identifier.</value>
+        Guid Id { get; }
+
+        /// <summary>Gets the environment's display name.</summary>
+        /// <value>The environment's display name.</value>
+        string DisplayName { get; }
+
+        /// <summary>Gets nsqlookupd instances associated with this environment.</summary>
+        /// <returns>The nsqlookupd instances associated with this environment.</returns>
+        Collection<Uri> GetNsqLookupds();
+    }
+}
