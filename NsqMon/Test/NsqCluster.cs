c33ee981eeb3710e6f21cdc34ed944d7af8d8bd4
Jud White <github@judsonwhite.com>
2015-08-19 03:53:14 -0500

    nsqmon: add localhost implementation for testing

---
+++ NsqMon/Test/NsqCluster.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NsqMon.Plugin.Interfaces;
+
+namespace NsqMon.Test
+{
+    public class NsqCluster : ICluster
+    {
+        private readonly Collection<IEnvironment> _environments;
+
+        public NsqCluster(Guid id, string displayName, IList<IEnvironment> environments)
+        {
+            Id = id;
+            DisplayName = displayName;
+            _environments = new Collection<IEnvironment>(environments);
+        }
+
+        /// <summary>Gets the cluster's user-specified unique identifier.</summary>
+        /// <value>The cluster's user-specified unique identifier.</value>
+        public Guid Id { get; private set; }
+
+        /// <summary>Gets the cluster's display name.</summary>
+        /// <value>The cluster's display name.</value>
+        public string DisplayName { get; }
+
+        /// <summary>Gets the environments associated with this cluster.</summary>
+        /// <returns>The environments associated with this cluster.</returns>
+        public Collection<IEnvironment> GetEnvironments()
+        {
+            return _environments;
+        }
+    }
+}
---
+++ NsqMon/Test/NsqEnvironment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NsqMon.Plugin.Interfaces;
+
+namespace NsqMon.Test
+{
+    public class NsqEnvironment : IEnvironment
+    {
+        private readonly Collection<Uri> _nsqLookupds;
+
+        public NsqEnvironment(Guid id, string displayName, IList<Uri> nsqLookupds)
+        {
+            Id = id;
+            DisplayName = displayName;
+            _nsqLookupds = new Collection<Uri>(nsqLookupds);
+        }
+
+        /// <summary>Gets the environment's display user-specified unique identifier.</summary>
+        /// <value>The environment's display user-specified unique identifier.</value>
+        public Guid Id { get; private set; }
+
+        /// <summary>Gets the environment's display name.</summary>
+        /// <value>The environment's display name.</value>
+        public string DisplayName { get; private set; }
+
+        /// <summary>Gets nsqlookupd instances associated with this environment.</summary>
+        /// <returns>The nsqlookupd instances associated with this environment.</returns>
+        public Collection<Uri> GetNsqLookupds()
+        {
+            return _nsqLookupds;
+        }
+    }
+}
