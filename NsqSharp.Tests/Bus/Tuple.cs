130cfc6c7257269947b6ddc62ddcabb4bb47db49
Jud White <github@judsonwhite.com>
2015-09-12 15:33:09 -0500

    Tests: move Tuple to separate file (.NET 3.5 support)

---
+++ NsqSharp.Tests/Bus/Tuple.cs
@@ -0,0 +1,19 @@
+﻿#if NETFX_3_5
+namespace System
+{
+    internal class Tuple<TItem1, TItem2>
+    {
+        private readonly TItem1 _item1;
+        private readonly TItem2 _item2;
+
+        public Tuple(TItem1 item1, TItem2 item2)
+        {
+            _item1 = item1;
+            _item2 = item2;
+        }
+
+        public TItem1 Item1 { get { return _item1; } }
+        public TItem2 Item2 { get { return _item2; } }
+    }
+}
+#endif
