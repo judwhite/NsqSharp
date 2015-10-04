2643815af0075a474f2f9c5f6428ff5fe732c28a
Jud White <github@judsonwhite.com>
2015-10-03 22:02:25 -0500

    Tests: add BusShutdownTest, TickerTest which repro'd buffered channel issue in #36

---
+++ NsqSharp.Tests/Bus/BusShutdownTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Newtonsoft.Json;
+using NsqSharp.Api;
+using NsqSharp.Bus;
+using NsqSharp.Bus.Configuration;
+using NsqSharp.Bus.Configuration.BuiltIn;
+using NsqSharp.Tests.Bus.TestFakes;
+using NsqSharp.Utils;
+using NsqSharp.Utils.Channels;
+using NsqSharp.Utils.Extensions;
+using NUnit.Framework;
+using StructureMap;
+
+namespace NsqSharp.Tests.Bus
+{
+#if !RUN_INTEGRATION_TESTS
+    [TestFixture(IgnoreReason = "NSQD Integration Test")]
+#else
+    [TestFixture]
+#endif
+    public class BusShutdownTest
+    {
+        private static readonly NsqdHttpClient _nsqdHttpClient;
+        private static readonly NsqLookupdHttpClient _nsqLookupdHttpClient;
+
+        private static readonly WaitGroup _wg = new WaitGroup();
+        private static bool _handlerFinished;
+
+        static BusShutdownTest()
+        {
+            _nsqdHttpClient = new NsqdHttpClient("http://127.0.0.1:4151", TimeSpan.FromSeconds(5));
+            _nsqLookupdHttpClient = new NsqLookupdHttpClient("http://127.0.0.1:4161", TimeSpan.FromSeconds(5));
+        }
+
+        [Test]
+        public void TestBusShutdown()
+        {
+            string topicName = string.Format("test_currentmessage_{0}", DateTime.Now.UnixNano());
+            const string channelName = "test_currentmessage";
+
+            var container = new Container();
+
+            _nsqdHttpClient.CreateTopic(topicName);
+            _nsqLookupdHttpClient.CreateTopic(topicName);
+
+            try
+            {
+                BusService.Start(new BusConfiguration(
+                    new StructureMapObjectBuilder(container),
+                    new NewtonsoftJsonSerializer(typeof(JsonConverter).Assembly),
+                    new MessageAuditorStub(),
+                    new MessageTypeToTopicDictionary(new Dictionary<Type, string> {
+                        { typeof(TestMessage), topicName }
+                    }),
+                    new HandlerTypeToChannelDictionary(new Dictionary<Type, string> {
+                        { typeof(TestMessageHandler), channelName }
+                    }),
+                    defaultNsqLookupdHttpEndpoints: new[] { "127.0.0.1:4161" },
+                    defaultThreadsPerHandler: 1,
+                    nsqConfig: new Config
+                    {
+                        LookupdPollJitter = 0,
+                        LookupdPollInterval = TimeSpan.FromSeconds(1)
+                    }
+                ));
+
+                var bus = container.GetInstance<IBus>();
+
+                _wg.Add(1);
+                bus.Send(new TestMessage());
+                _wg.Wait();
+
+                var stoppedChan = new Chan<bool>(1);
+                GoFunc.Run(() =>
+                           {
+                               var start = DateTime.Now;
+                               BusService.Stop();
+                               Console.WriteLine(string.Format("Shutdown occurred in {0}", DateTime.Now - start));
+                               stoppedChan.Send(true);
+                           });
+
+                bool timeout = false;
+                Select
+                    .CaseReceive(stoppedChan, o => { })
+                    .CaseReceive(Time.After(TimeSpan.FromMilliseconds(90000)), o => { timeout = true; })
+                    .NoDefault();
+
+                Assert.IsFalse(timeout, "timeout");
+                Assert.IsFalse(_handlerFinished, "handlerFinished");
+            }
+            finally
+            {
+                _nsqdHttpClient.DeleteTopic(topicName);
+                _nsqLookupdHttpClient.DeleteTopic(topicName);
+            }
+        }
+
+        private class TestMessage
+        {
+        }
+
+        private class TestMessageHandler : IHandleMessages<TestMessage>
+        {
+            public void Handle(TestMessage message)
+            {
+                _wg.Done();
+                Thread.Sleep(-1);
+                _handlerFinished = true;
+            }
+        }
+    }
+}
---
+++ NsqSharp.Tests/Utils/TickerTest.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NsqSharp.Utils;
+using NsqSharp.Utils.Channels;
+using NUnit.Framework;
+
+namespace NsqSharp.Tests.Utils
+{
+    [TestFixture]
+    public class TickerTest
+    {
+        [Test]
+        public void TestSingleTicker()
+        {
+            // arrange
+            var start = DateTime.Now;
+            var ticker = new Ticker(TimeSpan.FromSeconds(1));
+
+            // act
+            bool ok;
+            var sentAt = (DateTime)ticker.C.ReceiveOk(out ok);
+            var duration = DateTime.Now - start;
+            var offBy = DateTime.Now - sentAt;
+
+            ticker.Stop();
+
+            // assert
+            Assert.IsTrue(ok, "ok");
+            Assert.GreaterOrEqual(duration, TimeSpan.FromSeconds(1), "duration");
+            Assert.Less(duration, TimeSpan.FromSeconds(1.5), "duration");
+            Assert.Less(offBy, TimeSpan.FromSeconds(0.5), "offBy");
+        }
+
+        [Test]
+        public void TestDoubleTicker()
+        {
+            // arrange
+            var start = DateTime.Now;
+            var ticker = new Ticker(TimeSpan.FromSeconds(1));
+
+            // act
+            bool ok1;
+            var sentAt1 = (DateTime)ticker.C.ReceiveOk(out ok1);
+            var duration1 = DateTime.Now - start;
+            var offBy1 = DateTime.Now - sentAt1;
+
+            bool ok2;
+            var sentAt2 = (DateTime)ticker.C.ReceiveOk(out ok2);
+            var duration2 = DateTime.Now - start;
+            var offBy2 = DateTime.Now - sentAt2;
+
+            ticker.Stop();
+
+            // assert
+            Assert.IsTrue(ok1, "ok1");
+            Assert.GreaterOrEqual(duration1, TimeSpan.FromSeconds(1), "duration1");
+            Assert.Less(duration1, TimeSpan.FromSeconds(1.5), "duration1");
+            Assert.Less(offBy1, TimeSpan.FromSeconds(0.5), "offBy1");
+
+            Assert.IsTrue(ok2, "ok2");
+            Assert.GreaterOrEqual(duration2, TimeSpan.FromSeconds(2), "duration2");
+            Assert.Less(duration2, TimeSpan.FromSeconds(2.5), "duration2");
+            Assert.Less(offBy2, TimeSpan.FromSeconds(0.5), "offBy2");
+        }
+
+        [Test]
+        public void TestDoubleTickerWithStop()
+        {
+            // arrange
+            var start = DateTime.Now;
+            var ticker = new Ticker(TimeSpan.FromSeconds(1));
+
+            // act
+            bool ok1;
+            var sentAt1 = (DateTime)ticker.C.ReceiveOk(out ok1);
+            var duration1 = DateTime.Now - start;
+            var offBy1 = DateTime.Now - sentAt1;
+
+            ticker.Stop();
+
+            var newTicker = new Ticker(TimeSpan.FromSeconds(5));
+            bool? ok2 = null;
+            Select
+                .CaseReceiveOk(ticker.C, (d, b) => ok2 = false)
+                .CaseReceive(newTicker.C, _ => ok2 = true)
+                .NoDefault();
+
+            newTicker.Stop();
+
+            // assert
+            Assert.IsTrue(ok1, "ok1");
+            Assert.GreaterOrEqual(duration1, TimeSpan.FromSeconds(1), "duration1");
+            Assert.Less(duration1, TimeSpan.FromSeconds(1.5), "duration1");
+            Assert.Less(offBy1, TimeSpan.FromSeconds(0.5), "offBy1");
+
+            Assert.IsNotNull(ok2, "ok2");
+            Assert.IsTrue(ok2.Value, "ok2");
+        }
+
+        [Test]
+        public void TestTickerLoopWithExitChan()
+        {
+            var start = DateTime.Now;
+            var ticker = new Ticker(TimeSpan.FromSeconds(1));
+
+            var listOfTimes = new List<TimeSpan>();
+            var exitChan = new Chan<bool>();
+            var lookupdRecheckChan = new Chan<bool>();
+            bool doLoop = true;
+            using (var select =
+                    Select
+                        .CaseReceive(ticker.C, o => listOfTimes.Add(DateTime.Now - start))
+                        .CaseReceive(lookupdRecheckChan, o => listOfTimes.Add(DateTime.Now - start))
+                        .CaseReceive(exitChan, o => doLoop = false)
+                        .NoDefault(defer: true))
+            {
+                // ReSharper disable once LoopVariableIsNeverChangedInsideLoop
+                while (doLoop)
+                {
+                    select.Execute();
+                    if (listOfTimes.Count >= 10)
+                    {
+                        GoFunc.Run(() => exitChan.Send(true));
+                    }
+                }
+            }
+
+            ticker.Stop();
+
+            var duration = DateTime.Now - start;
+            Assert.AreEqual(10, listOfTimes.Count, "listOfTimes.Count");
+            Assert.GreaterOrEqual(duration, TimeSpan.FromSeconds(10), "duration");
+            Assert.Less(duration, TimeSpan.FromSeconds(11));
+        }
+
+        [Test]
+        public void TestTickerLoopWithNemesisChan()
+        {
+            var start = DateTime.Now;
+            var ticker = new Ticker(TimeSpan.FromSeconds(1));
+
+            var listOfTimes = new List<TimeSpan>();
+            var exitChan = new Chan<bool>();
+            var lookupdRecheckChan = new Chan<bool>();
+            bool doLoop = true;
+            using (var select =
+                    Select
+                        .CaseReceive(ticker.C,
+                                     o =>
+                                     {
+                                         Console.WriteLine("Received tick");
+                                         listOfTimes.Add(DateTime.Now - start);
+
+                                         if (listOfTimes.Count == 5)
+                                         {
+                                             GoFunc.Run(() => lookupdRecheckChan.Send(true));
+                                         }
+                                     })
+                        .CaseReceive(lookupdRecheckChan,
+                                     o =>
+                                     {
+                                         Console.WriteLine("Nemesis");
+                                         Thread.Sleep(5000);
+                                     })
+                        .CaseReceive(exitChan, o => doLoop = false)
+                        .NoDefault(defer: true))
+            {
+                // ReSharper disable once LoopVariableIsNeverChangedInsideLoop
+                while (doLoop)
+                {
+                    select.Execute();
+                    if (listOfTimes.Count >= 10)
+                    {
+                        GoFunc.Run(() => exitChan.Send(true));
+                    }
+                }
+            }
+
+            ticker.Stop();
+
+            var duration = DateTime.Now - start;
+            Console.WriteLine(duration);
+            Assert.AreEqual(10, listOfTimes.Count, "listOfTimes.Count");
+            Assert.GreaterOrEqual(duration, TimeSpan.FromSeconds(14), "duration");
+            Assert.Less(duration, TimeSpan.FromSeconds(17));
+        }
+
+        [Test]
+        public void TestTickerLoopWithNemesisBufferedChan()
+        {
+            var start = DateTime.Now;
+            var ticker = new Ticker(TimeSpan.FromSeconds(1));
+
+            int x = 0;
+            var listOfTimes = new List<TimeSpan>();
+            var exitChan = new Chan<bool>();
+            var lookupdRecheckChan = new Chan<bool>(bufferSize: 1);
+            bool doLoop = true;
+            using (var select =
+                    Select
+                        .CaseReceive(ticker.C,
+                                     o =>
+                                     {
+                                         Console.WriteLine("Received tick");
+                                         listOfTimes.Add(DateTime.Now - start);
+
+                                         if (listOfTimes.Count == 5)
+                                         {
+                                             Select
+                                                 .CaseSend(lookupdRecheckChan, true)
+                                                 .NoDefault(defer: false);
+                                         }
+                                     })
+                        .CaseReceive(lookupdRecheckChan,
+                                     o =>
+                                     {
+                                         Console.WriteLine("Nemesis");
+                                         for (int i = 0; i < 5; i++)
+                                         {
+                                             Thread.Sleep(1000);
+                                             Console.Write(".");
+                                         }
+                                         Console.WriteLine();
+                                     })
+                        .CaseReceive(exitChan, o => doLoop = false)
+                        .NoDefault(defer: true))
+            {
+                // ReSharper disable once LoopVariableIsNeverChangedInsideLoop
+                while (doLoop)
+                {
+                    Console.WriteLine("start {0}", x);
+                    if (x == 7)
+                    {
+                        Console.WriteLine("pause");
+                    }
+                    select.Execute();
+                    Console.WriteLine("finish {0}", x);
+                    x++;
+                    if (listOfTimes.Count >= 10)
+                    {
+                        GoFunc.Run(() => exitChan.Send(true));
+                    }
+                }
+            }
+
+            ticker.Stop();
+
+            var duration = DateTime.Now - start;
+            Console.WriteLine(duration);
+            Assert.AreEqual(10, listOfTimes.Count, "listOfTimes.Count");
+            Assert.GreaterOrEqual(duration, TimeSpan.FromSeconds(14), "duration");
+            Assert.Less(duration, TimeSpan.FromSeconds(17));
+        }
+    }
+}
