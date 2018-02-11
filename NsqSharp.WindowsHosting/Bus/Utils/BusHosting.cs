7fca8a83b7c8a6bbe183051cc439a59ef1742c43
Kirill Bykov <crazybert@gmail.com>
2018-02-10 18:05:43 -0600

    NsqSharp.WindowsHosting

---
+++ NsqSharp.WindowsHosting/Bus/Utils/BusHosting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NsqSharp.Bus.Utils;
+namespace NsqSharp.Bus
+{
+
+    public static class BusHosting
+    {
+        /// <summary>
+        /// <c>true</c> if the process is running in a console window.
+        /// </summary>
+        public static bool IsConsoleMode 
+        {
+            get
+            {
+                return (NativeMethods.GetConsoleWindow() != IntPtr.Zero);
+            }
+        }
+    }
+}
